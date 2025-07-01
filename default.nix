c7801a3bf64de6ed25289bb231f9e9c4a670ee21
Socheat Sok <socheatsok78@gmail.com>
2025-07-01 11:22:26 +0700

    Revert "Fix default.nix"
    This reverts commit f59d72cb89cbf54f84d8f87ed7ca8502eefb11cc.

--- default.nix
+++ default.nix
@@ -9,20 +9,26 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
-rec {
+let
+  # NUR
+  maintainer = import ./maintainers; # list of maintainers
+
+  # Call package with the maintainer
+  # This is a convenience function to avoid repeating the maintainer argument
+  # in every package definition.
+  callPackage = path: { }: pkgs.callPackage path { inherit maintainer; };
+in
+{
 
   # The `lib`, `modules`, and `overlays` names are special
   lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  # NUR
-  maintainer = import ./maintainers; # list of maintainers
-
   example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 
-  certstrap = pkgs.callPackage ./pkgs/certstrap { inherit maintainer; };
-  pangolin-installer = pkgs.callPackage ./pkgs/pangolin-installer { inherit maintainer; };
+  certstrap = callPackage ./pkgs/certstrap { };
+  pangolin-installer = callPackage ./pkgs/pangolin-installer { };
 }
