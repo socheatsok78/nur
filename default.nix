f59d72cb89cbf54f84d8f87ed7ca8502eefb11cc
Socheat Sok <socheatsok78@gmail.com>
2025-07-01 11:19:47 +0700

    Fix default.nix

--- default.nix
+++ default.nix
@@ -9,26 +9,20 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
-let
-  # NUR
-  maintainer = import ./maintainers; # list of maintainers
-
-  # Call package with the maintainer
-  # This is a convenience function to avoid repeating the maintainer argument
-  # in every package definition.
-  callPackage = path: { }: pkgs.callPackage path { inherit maintainer; };
-in
-{
+rec {
 
   # The `lib`, `modules`, and `overlays` names are special
   lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
+  # NUR
+  maintainer = import ./maintainers; # list of maintainers
+
   example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 
-  certstrap = callPackage ./pkgs/certstrap { };
-  pangolin-installer = callPackage ./pkgs/pangolin-installer { };
+  certstrap = pkgs.callPackage ./pkgs/certstrap { inherit maintainer; };
+  pangolin-installer = pkgs.callPackage ./pkgs/pangolin-installer { inherit maintainer; };
 }
