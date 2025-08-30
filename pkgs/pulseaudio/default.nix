be29147f3e2df459390bc94d14a55eb8259fda01
Socheat Sok <socheatsok78@gmail.com>
2025-08-30 16:54:29 +0700

    Fix pulseaudio syntax

--- pkgs/pulseaudio/default.nix
+++ pkgs/pulseaudio/default.nix
@@ -117,7 +117,7 @@ stdenv.mkDerivation rec {
 
   propagatedBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ libcap ];
 
-  preConfigure = "" + lib.optionals stdenv.hostPlatform.isDarwin ''
+  preConfigure = "" + lib.optionalString stdenv.hostPlatform.isDarwin ''
       # Restore coreaudio module as default on macOS
       sed -i "s/cdata.set('HAVE_COREAUDIO', 0)/cdata.set('HAVE_COREAUDIO', 1)/" meson.build
     '';
