7f49fcc352602abd558cbc3cdf0b30c39dca40b7
Socheat <github@socheat.dev>
2026-08-07 17:24:15 +0700

    update default.nix

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 # This file describes your repository contents.
 # It should return a set of nix derivations
-# and optionally the special attributes `lib`, `modules` and `overlays`.
+# and optionally the special attributes `lib`, `overlays`,
+# `nixosModules`, `homeModules`, `darwinModules` and `flakeModules`.
 # It should NOT import <nixpkgs>. Instead, you should take pkgs as an argument.
 # Having pkgs default to <nixpkgs> is fine though, and it lets you use short
 # commands such as:
@@ -10,9 +11,13 @@
   pkgs ? import <nixpkgs> { },
 }:
 rec {
-  # The `lib`, `modules`, and `overlays` names are special
+  # The `lib`, `overlays`, `nixosModules`, `homeModules`,
+  # `darwinModules` and `flakeModules` names are special
   lib = import ./lib { inherit pkgs; }; # functions
-  modules = import ./modules; # NixOS modules
+  # nixosModules = { }; # NixOS modules
+  # homeModules = { }; # Home Manager modules
+  darwinModules = import ./darwin-modules; # nix-darwin modules
+  # flakeModules = { }; # flake-parts modules
   overlays = import ./overlays; # nixpkgs overlays
 
   # Packages
